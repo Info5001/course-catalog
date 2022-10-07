aae604aa9e05ddac5a0bfc33f35b120dc2830049
archil-student <lelashvili.a@northeastern.edu>
2022-10-07 16:50:01 -0400

    Template prep

---
+++ src/model/CourseCatalog.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.ArrayList;
+
+public class CourseCatalog {
+    ArrayList<Course> courselist;
+
+    public CourseCatalog(){
+        courselist = new ArrayList<Course>();
+    }
+
+    
+}
