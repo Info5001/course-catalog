16e76744ddc211c735b28e256079f005fb32f406
Archil Lelashvili <lelashvili.a@northeastern.edu>
2023-10-02 14:57:33 -0400

    Solution developed in lab

--- src/model/Department.java
+++ src/model/Department.java
@@ -1,5 +1,23 @@
 package model;
 
 public class Department {
-    
+    String name;
+    String nameOfHead;
+
+    public Department(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getNameOfHead() {
+        return nameOfHead;
+    }
+    public void setNameOfHead(String nameOfHead) {
+        this.nameOfHead = nameOfHead;
+    }
 }
