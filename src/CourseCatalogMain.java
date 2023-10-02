8be563b4baa827f0deb4e82eeb6b3f442c982a1f
Archil Lelashvili <lelashvili.a@northeastern.edu>
2023-10-02 14:56:24 -0400

    Empty template

--- src/CourseCatalogMain.java
+++ src/CourseCatalogMain.java
@@ -1,6 +1,6 @@
-package ui;
 
-public class Main {
+
+public class CourseCatalogMain {
     public static void main(String[] args) throws Exception {
         System.out.println("Hey lets develop a course catalog!");
     }
