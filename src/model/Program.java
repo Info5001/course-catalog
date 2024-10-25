621319a2a0aad6d11ccbd56b0877062fd27efb9b
archil-nu <a.lelashvili@northeastern.edu>
2024-10-25 17:55:36 -0400

    Some cleanup

--- src/model/Program.java
+++ src/model/Program.java
@@ -1,22 +1,25 @@
 package model;
 
-public class Department {
+public class Program {
     String name;
     String nameOfHead;
 
-    public Department(String name) {
+    public Program(String name) {
         this.name = name;
     }
 
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getNameOfHead() {
         return nameOfHead;
     }
+
     public void setNameOfHead(String nameOfHead) {
         this.nameOfHead = nameOfHead;
     }
